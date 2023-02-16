507d68577debd0d076ff787a39be47de9f3709b4
Unibajt Technologies <p.plichta@emerc.pl>
2023-02-16 17:19:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,33 @@
-<?php
-// Sprawdzanie, czy użytkownik jest zalogowany jako administrator
-session_start();
-if (!isset($_SESSION['admin'])) {
-    header('Location: login.php');
-}
-
-// Sprawdzanie, czy formularz został wysłany
-if (isset($_POST['submit'])) {
-    // Łączenie z bazą danych
-    include 'dbconfig.php';
-
-    // Pobieranie danych z formularza
-    $title = $_POST['title'];
-    $content = $_POST['content'];
-    $type = $_POST['type'];
-
-    // Walidacja danych
-    if (empty($title) || empty($content) || empty($type)) {
-        $error = 'Proszę wypełnić wszystkie pola.';
-    } else {
-        // Dodawanie artykułu lub poradnika do bazy danych
-        $stmt = $pdo->prepare("INSERT INTO articles (title, content, type) VALUES (?, ?, ?)");
-        $stmt->execute([$title, $content, $type]);
-
-        // Przekierowanie do strony głównej
-        header('Location: index.php');
-    }
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dodaj artykuł lub poradnik - Panel administratora</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Strona z artykułami i poradnikami</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Dodaj artykuł lub poradnik</h1>
-        <?php if (isset($error)) { ?>
-            <p class="error"><?php echo $error; ?></p>
-        <?php } ?>
-        <form method="post">
-            <label>Tytuł:</label>
-            <input type="text" name="title">
-            <br>
-            <label>Treść:</label>
-            <textarea name="content"></textarea>
-            <br>
-            <label>Typ:</label>
-            <select name="type">
-                <option value="">Wybierz typ</option>
-                <option value="artykuł">Artykuł</option>
-                <option value="poradnik">Poradnik</option>
-            </select>
-            <br>
-            <input type="submit" name="submit" value="Dodaj">
-        </form>
-        <a href="logout.php">Wyloguj się</a>
-    </div>
+	<header>
+		<h1>Strona z artykułami i poradnikami</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Strona główna</a></li>
+				<li><a href="login.php">Zaloguj się</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h2>Artykuły</h2>
+		<ul id="articles">
+			<!-- tu będą wyświetlane artykuły z bazy danych -->
+		</ul>
+		<h2>Poradniki</h2>
+		<ul id="guides">
+			<!-- tu będą wyświetlane poradniki z bazy danych -->
+		</ul>
+	</main>
+	<footer>
+		<p>Strona z artykułami i poradnikami &copy; 2023</p>
+	</footer>
+	<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
